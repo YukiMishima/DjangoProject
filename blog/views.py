bc0dd8bf80fe79dbb286525bf3efd78c3a8d13d6
yuki <amagai05.14@gmail.com>
2019-05-08 09:55:25 +0900

    get created_time and char_count, and transfer the list to js

--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+import os
 from django.http import HttpResponse
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    # data = {
+    #             "1530370800": 1,  # 2018 / 07 / 01
+    #             "1530457200": 3,  # 2018 / 07 / 02
+    #             "1533049200": 5,  # 2018 / 08 / 01
+    #             "1533135600": 7,  # 2018 / 08 / 02
+    #             "1546268400": 10  # 2019 / 01 / 01
+    # }
+    unix_time = [post.published_date.timestamp() for post in posts]
+    char_count = [len(post.text) for post in posts]
+    data = dict(zip(unix_time, char_count))
+    return render(request, 'blog/post_list.html', {
+        'posts': posts,
+        'data': data,
+    })
 
 
 def post_browse(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/post_browse.html', {'post': post})
+    return render(request, 'blog/post_browse.html', {
+        'post': post,
+    })
 
 
 def post_edit(request, post_id=None):
