47ccd01a873795c0de1303ba5ad2755bfbcf9d95
yuki <amagai05.14@gmail.com>
2019-05-08 20:39:53 +0900

    add markdownx

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    # text = models.TextField()
+    text = MarkdownxField('本文', help_text='Markdown形式で書いてください. ')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -14,6 +17,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def text_to_markdown(self):
+        return markdownify(self.text)
 
     def __str__(self):
         return self.title
